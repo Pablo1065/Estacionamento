6de36b4d6df4516090d12cc18b614f5ebdc36dcb
pablo <pablo@DESKTOP-TNL8CKM>
2022-11-10 16:42:58 -0300

    Semana 5 - Listagem de Vagas e Motoristas

--- Estacionamento/src/connection/ConnectionFactory.java
+++ Estacionamento/src/connection/ConnectionFactory.java
@@ -8,9 +8,9 @@ import java.sql.ResultSet;
       
 public class ConnectionFactory {
     private static final String DRIVER = "com.mysql.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3307/estacionamento";
+    private static final String URL = "jdbc:mysql://localhost:3306/estacionamento";
     private static final String USER = "root";
-    private static final String PASS = "";
+    private static final String PASS = "root1234";
     
     public static Connection getConnection() {
         try{
--- Estacionamento/src/model/dao/MotoristaDAO.java
+++ Estacionamento/src/model/dao/MotoristaDAO.java
@@ -7,6 +7,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MotoristaDAO {
     
@@ -14,7 +17,7 @@ public class MotoristaDAO {
         Connection con = ConnectionFactory.getConnection();
         PreparedStatement stmt = null;
         try{
-            stmt = con.prepareStatement("INSERT INT motorista (nome, homem, rg, cpf, celular, email, senha) VALUES (?, ?, ?, ?, ?, ?, ?)");
+            stmt = con.prepareStatement("INSERT INTO motorista (nome, homem, rg, cpf, celular, email, senha) VALUES (?, ?, ?, ?, ?, ?, ?);");
             stmt.setString(1, m.getNome());
             stmt.setBoolean(2, m.isHomem());
             stmt.setString(3, m.getRg());
@@ -24,11 +27,38 @@ public class MotoristaDAO {
             stmt.setString(7, m.getSenha());
             
             stmt.executeUpdate();
-            JOptionPane.showMessageDialog(null, "Vaga salva com sucesso");
+            JOptionPane.showMessageDialog(null, "Motorista salva com sucesso");
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Deu PAU irmão :" + e);
         } finally {
             ConnectionFactory.closeConnection(con, stmt);
         }
+    }
+     public List<Motorista> read(){
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        List<Motorista> motoristas = new ArrayList<>();
+        try{
+            stmt = con.prepareStatement("SELECT * FROM motorista;");
+            rs = stmt.executeQuery();
+            while(rs.next()){
+                Motorista m = new Motorista();
+                m.setIdMotorista(rs.getInt("idMotorista"));
+                m.setNome(rs.getString("nome"));
+                m.setHomem(rs.getBoolean("homem"));
+                m.setRg(rs.getString("rg"));
+                m.setCpf(rs.getString("cpf"));
+                m.setCelular(rs.getString("celular"));
+                m.setEmail(rs.getString("email"));
+                m.setSenha(rs.getString("senha"));
+                motoristas.add(m);
+            }
+        } catch (SQLException e){
+            throw new RuntimeException("Erro ao buscar os dados: ", e);
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt, rs);
+        }
+        return motoristas;
     }
 }
